0a58778e3d62ef89d97bc448871f963bcc32f449
T. Hikawa <s1210822@u.tsukuba.ac.jp>
2017-07-03 21:40:15 +0900

    update init.el

--- init.el
+++ init.el
@@ -2,24 +2,37 @@
 ;; type
 (define-key global-map (kbd "C-z") 'undo)
 (define-key global-map (kbd "C-v") 'yank)
-(define-key global-map (kbd "C-y") ' yank)
+(define-key global-map (kbd "C-y") 'yank)
+
 ;; cursor
+;; use Vim!
 (define-key global-map (kbd "C-h") 'backward-char)
 (define-key global-map (kbd "C-l") 'forward-char)
 (define-key global-map (kbd "C-j") 'next-line)
 (define-key global-map (kbd "C-k") 'previous-line)
+
 ;; scroll
-(define-key global-map (kbd "C-n") 'scroll-up-command)
 (define-key global-map (kbd "C-p") 'scroll-down-command)
+(define-key global-map (kbd "C-n") 'scroll-up-command)
 (define-key global-map (kbd "C-f") 'recenter-top-bottom)
-;; buffer
-(define-key global-map (kbd "M-n") 'next-buffer)
+
+;; buffer && window
+;; select buffer
 (define-key global-map (kbd "M-p") 'previous-buffer)
+(define-key global-map (kbd "M-n") 'next-buffer)
 (define-key global-map (kbd "C-r") 'eval-buffer)
+;; move window
+(define-key global-map (kbd "C-c h") 'windmove-left)
+(define-key global-map (kbd "C-c j") 'windmove-down)
+(define-key global-map (kbd "C-c k") 'windmove-up)
+(define-key global-map (kbd "C-c l") 'windmove-right)
+;; select window size
+(define-key global-map (kbd "C-c C-r") 'window-resizer)
+
+
 ;; others
 (define-key global-map (kbd "C-b") 'describe-bindings)
-(define-key global-map (kbd "C-x C-m") 'magit-status)
-(define-key global-map (kbd "C-SPC") 'nil)
+(define-key global-map (kbd "C-SPC") 'nil) ;; kill to select input method
 (define-key global-map (kbd "C-q") ' query-replace)
 (define-key global-map (kbd "RET") ' newline)
 
@@ -114,3 +127,65 @@
 (add-hook 'yatex-mode-hook'(lambda ()(setq auto-fill-function nil)))
 (add-hook 'latex-mode-hook 'turn-on-reftex)
 (add-hook 'yatex-mode-hook 'turn-on-reftex)
+
+
+;; web-mode setting
+(require 'web-mode)
+;; extensions
+(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ctp\\'"     . web-mode))
+;; indents
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-html-offset   4)
+  (setq web-mode-css-offset    4)
+  (setq web-mode-script-offset 4)
+  (setq web-mode-php-offset    4)
+  (setq web-mode-java-offset   4)
+  (setq web-mode-asp-offset    4)
+  (setq indent-tabs-mode t)
+  (setq tab-width 4)
+;; auto tag closing
+  ;0=no auto-closing
+  ;1=auto-close with </
+  ;2=auto-close with > and </
+  (setq web-mode-tag-auto-close-style 2))
+
+  
+(add-hook 'web-mode-hook 'web-mode-hook)
+
+;; 
+
+(defun window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        c)
+    (catch 'end-flag
+      (while t
+        (message "size[%dx%d]"
+                 (window-width) (window-height))
+        (setq c (read-char))
+        (cond ((= c ?l)
+               (enlarge-window-horizontally dx))
+              ((= c ?h)
+               (shrink-window-horizontally dx))
+              ((= c ?j)
+               (enlarge-window dy))
+              ((= c ?k)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               (message "Quit")
+               (throw 'end-flag t)))))))
