6fa0db557f300e81107f8e8972c814879413825c
23prime <23.prime.37@gmail.com>
2018-06-21 00:19:28 +0900

    Add node.js REPL

--- inits/13-js.el
+++ inits/13-js.el
@@ -3,6 +3,24 @@
 ;;;;;;;;
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-hook 'js2-mode-hook '(lambda () (setq tab-width 2)))
+
+(require 'nodejs-repl)
+
+(defun nodejs-repl-load-current-buffer ()
+  (interactive)
+  (nodejs-repl-load-file (buffer-file-name ()))
+)
+
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (define-key js2-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-expression)
+            (define-key js2-mode-map (kbd "C-c C-j") 'nodejs-repl-send-line)
+            (define-key js2-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
+            (define-key js2-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
+            (define-key js2-mode-map (kbd "C-c C-p") 'nodejs-repl-load-current-buffer)
+            (define-key js2-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
+
 
 ;;;;;;;;;
 ;; Web ;;
@@ -16,6 +34,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ctp\\'"     . web-mode))
+
 ;; indents
 (defun web-mode-hook ()
   "Hooks for Web mode."
@@ -26,7 +45,7 @@
   (setq web-mode-java-offset   4)
   (setq web-mode-asp-offset    4)
   (setq indent-tabs-mode t)
-  (setq tab-width 4)
+  (setq tab-width 2)
 ;; auto tag closing
   ;0=no auto-closing
   ;1=auto-close with </
