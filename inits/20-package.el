60b0caf9c293e1dbca079f694101c0170e357d25
23prime <23.prime.37@gmail.com>
2018-07-20 16:48:17 +0900

    Fix

--- inits/20-package.el
+++ inits/20-package.el
@@ -1,6 +1,6 @@
 (require 'package)
-(add-to-list 'package-archives' ("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives' ("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 (when (version < emacs-version "27.0")
   (package-initialize)
